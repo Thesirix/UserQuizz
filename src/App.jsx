89f930e5de38547aed960465e3dbfc6f62efe282
thesirix <renaud.d.mercier@gmail.com>
2024-04-28 14:13:55 +0200

    first cut

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Quiz from './componants/Quiz'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+ 
+
+  return (
+    <>
+<Quiz/>
+    </>
+  )
+}
+
+export default App
